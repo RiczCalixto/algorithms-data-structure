6009402907122479924c3a9a853e03ace8f956f6
Ricardo Calixto <rcalixtos@gmail.com>
2019-08-25 17:35:13 -0300

    fizzBuzz problem

--- exercicios-quokka.js
+++ exercicios-quokka.js
@@ -111,3 +111,20 @@ function maxChar(str) {
 
 console.log(maxChar('casa caralhuddd'))
 
+function fizzBuzz(n) {
+  for(let i = 1; i <= n; i++){
+    if(i%3 === 0 && i%5 === 0) {
+     console.log('fizzbuss')
+    }
+    else if(i%3 === 0) {
+      console.log('fizz')
+    } else if (i%5 === 0) {
+      console.log('buzz')
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+console.log(fizzBuzz(15))
+
